b7eee8001847167ef5339c0a7802650887427e21
AKIBOY\ahmed <ahmedmohamed031098@gmail.com>
2020-11-25 10:48:19 +0100

    no changes

---
+++ rgb_sensor/main/rgb_sensor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+void app_main(void){ 
+    
+    while(1){
+    
+    }
+}
