5cd476891825d8768737fd24dcaee4cba30469b0
AKIBOY\ahmed <ahmedmohamed031098@gmail.com>
2020-12-23 19:20:31 +0100

    RGB sensor works

--- rgb_sensor/main/rgb_sensor.c
+++ rgb_sensor/main/rgb_sensor.c
@@ -45,6 +45,25 @@
 #define TCS34725_GDATAH         0x19             /**< Green channel data high byte */
 #define TCS34725_BDATAL         0x1A             /**< Blue channel data low byte */
 #define TCS34725_BDATAH         0x1B             /**< Blue channel data high byte */
+
+
+void i2c_master_init(){
+  // configure the i2c controller 0 in master mode, normal speed
+	i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = I2C_SCL_IO;
+	conf.scl_io_num = I2C_SDA_IO;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.master.clk_speed = 100000;
+	ESP_ERROR_CHECK(i2c_param_config(I2C_PORT_NUM, &conf));
+	printf("- i2c controller configured\r\n");
+	
+	// install the driver
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_PORT_NUM, I2C_MODE_MASTER, 0, 0, 0));
+	printf("- i2c driver installed\r\n\r\n");
+    
+}
 /**
  * @brief test code to read i2c slave device with registered interface
  * _______________________________________________________________________________________________________
@@ -54,6 +73,7 @@
  */
 static esp_err_t i2c_master_read_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_rd, size_t size)
 {
+  //uint8_t throwaway=0;
     if (size == 0) {
         return ESP_OK;
     }
@@ -62,7 +82,7 @@ static esp_err_t i2c_master_read_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr,
     // first, send device address (indicating write) & register to be read
     i2c_master_write_byte(cmd, ( i2c_addr << 1 ), ACK_CHECK_EN);
     // send register we want
-    i2c_master_write_byte(cmd, i2c_reg, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd,0x80| i2c_reg, ACK_CHECK_EN);
     // Send repeated start
     i2c_master_start(cmd);
     // now send device address (indicating read) & read data
@@ -94,7 +114,7 @@ static esp_err_t i2c_master_write_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr
     // first, send device address (indicating write) & register to be written
     i2c_master_write_byte(cmd, ( i2c_addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
     // send register we want
-    i2c_master_write_byte(cmd, i2c_reg, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, 0x80|i2c_reg, ACK_CHECK_EN);
     // write the data
     i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
     i2c_master_stop(cmd);
@@ -115,74 +135,71 @@ esp_err_t wrtcs34725x( uint8_t reg, uint8_t *pdata, uint8_t count )
 {
 	return( i2c_master_write_slave_reg( I2C_PORT_NUM, TCS34725_ADDRESS,  reg, pdata, count ) );
 }
+void tcs34725_enable(){
+  uint8_t value;
+    // Select enable register(0x00)
+	  // Power ON, RGBC enable, wait time disable(0x03)
+    value= (0x01);
+    wrtcs34725x( TCS34725_ENABLE,&(value),1);
+    vTaskDelay(3 / portTICK_RATE_MS);
+    value= (0x03);
+    wrtcs34725x( TCS34725_ENABLE,&(value),1);
+}
+void tcs34725_init(){
+  uint8_t value;
+  // Select ALS time register(0x01)
+	  // Atime = 700 ms(0x00)
+    value= (0x00);
+    wrtcs34725x( TCS34725_ATIME,&(value),1);
+    // Select control register(0x0F)
+	  // AGAIN = 1x(0x00)
+    value= (0x00);
+    wrtcs34725x( TCS34725_CONTROL,&(value),1);
+}
 
 
 void app_main(void){ 
-  // configure the i2c controller 0 in master mode, normal speed
-	i2c_config_t conf;
-	conf.mode = I2C_MODE_MASTER;
-	conf.sda_io_num = I2C_SCL_IO;
-	conf.scl_io_num = I2C_SDA_IO;
-	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-	conf.master.clk_speed = 100000;
-	ESP_ERROR_CHECK(i2c_param_config(I2C_PORT_NUM, &conf));
-	printf("- i2c controller configured\r\n");
-	
-	// install the driver
-	ESP_ERROR_CHECK(i2c_driver_install(I2C_PORT_NUM, I2C_MODE_MASTER, 0, 0, 0));
-	printf("- i2c driver installed\r\n\r\n");
-  int devices_found = 0;
-    uint8_t value;
-    // Select enable register(0x00)
-	  // Power ON, RGBC enable, wait time disable(0x03)
-    value= 0x03;
-    wrtcs34725x( TCS34725_ENABLE,&value,1);
-    // Select ALS time register(0x01)
-	  // Atime = 700 ms(0x00)
-    value= 0x00;
-    wrtcs34725x( TCS34725_ATIME,&value,1);
-    // Select Wait Time register(0x03)
-	  // WTIME : 2.4ms(0xFF) 
-    value= 0xFF;
-    wrtcs34725x( TCS34725_WTIME,&value,1);
-    // Select control register(0x0F)
-	  // AGAIN = 1x(0x00)
-    value= 0x00;
-    wrtcs34725x( TCS34725_CONTROL,&value,1);
-    // Read 8 bytes of data from register(0x94)
-	  // cData lsb, cData msb, red lsb, red msb, green lsb, green msb, blue lsb, blue msb
-    // wait for the sensor (50ms)
-    //vTaskDelay(50 / portTICK_RATE_MS);
-    uint8_t c=0;
-    uint8_t r=0;
-    uint8_t g=0;
-    uint8_t b=0;
-    rdtcs34725x( TCS34725_CDATAL,&c,1);
-    rdtcs34725x( TCS34725_RDATAL,&r,1);
-    rdtcs34725x( TCS34725_GDATAL,&g,1);
-    rdtcs34725x( TCS34725_BDATAL,&b,1);
-     for(int address = 1; address < 127; address++) {
-	
-		// create and execute the command link
-		i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-		i2c_master_start(cmd);
-		i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, true);
-		i2c_master_stop(cmd);
-		if(i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS) == ESP_OK) {
-			printf("-> found device with address 0x%02x\r\n", address);
-			devices_found++;
-		}
-		i2c_cmd_link_delete(cmd);
-	}
-	if(devices_found == 0) printf("\r\n-> no devices found\r\n");
-	printf("\r\n...scan completed!\r\n");
+    i2c_master_init();
+    tcs34725_enable();
+    tcs34725_init();
+    uint8_t cl=0;
+    uint8_t rl=0;
+    uint8_t gl=0;
+    uint8_t bl=0;
+    uint8_t ch=0;
+    uint8_t rh=0;
+    uint8_t gh=0;
+    uint8_t bh=0;
+    uint8_t e;
+    uint16_t c;
+    uint16_t r;
+    uint16_t g;
+    uint16_t b;
     while(1){
-    printf("red :%d\n", c);
-    printf("red :%d\n", r);
+	  // cData lsb, cData msb, red lsb, red msb, green lsb, green msb, blue lsb, blue msb
+    //wrtcs34725x(TCS34725_ENABLE,0x03,1);
+    rdtcs34725x(TCS34725_ENABLE,&e,1);
+    //wrtcs34725x(TCS34725_CDATAH,0x00,1);
+    rdtcs34725x( TCS34725_CDATAL,&cl,1);
+    rdtcs34725x( TCS34725_CDATAH,&ch,1);
+    //wrtcs34725x(TCS34725_CDATAH,0x00,1);
+    rdtcs34725x( TCS34725_RDATAL,&rl,1);
+    rdtcs34725x( TCS34725_RDATAH,&rh,1);
+    //wrtcs34725x(TCS34725_CDATAH,0x00,1);
+    rdtcs34725x( TCS34725_GDATAL,&gl,1);
+    rdtcs34725x( TCS34725_GDATAH,&gh,1);
+    rdtcs34725x( TCS34725_BDATAL,&bl,1);
+    rdtcs34725x( TCS34725_BDATAH,&bh,1);
+    c = ((uint16_t) ch << 8) | (uint16_t) cl;
+    r = ((uint16_t) rh << 8) | (uint16_t) rl;
+    g = ((uint16_t) gh << 8) | (uint16_t) gl;
+    b = ((uint16_t) bh << 8) | (uint16_t) bl;
+    printf("Enable Reg: %d \n",e);
+    printf("clear :%d\n",c);
+    printf("red:%d\n", r);
     printf("green :%d\n", g);
     printf("blue :%d\n", b);
-    
+    vTaskDelay(800 / portTICK_RATE_MS);
   
     }
 }
